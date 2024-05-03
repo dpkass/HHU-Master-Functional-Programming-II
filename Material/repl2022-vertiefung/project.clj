72aa804a4ec806b647277bc118861af74e071181
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2024-05-03 10:53:41 +0200

    Add - REPL Session - Transients

--- Material/repl2022-vertiefung/project.clj
+++ Material/repl2022-vertiefung/project.clj
@@ -3,21 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;[org.clojure/clojure "1.8.0"]
-
-                 ;; 05 Rekursion
-                 [org.clojure/tools.trace "0.7.9"]
-                 ;; 06 Polymorphism
-                 [instaparse "1.4.1"]
-
-                 ;; 13 Monaden
-                 ;; [org.clojure/clojure "1.7.0"]
-                 [org.clojure/algo.monads "0.1.5"]
-
+  :dependencies [
                  ;; 15 test.check
                  ;; braucht älteres leiningen (<= 2.8.1)
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/test.check "0.7.0"] ;; aktuelle Version: [org.clojure/test.check "0.9.0"]
+                 ;[org.clojure/clojure "1.5.1"]
+                 [org.clojure/test.check "0.9.0"]
+                 ;; aktuelle Version: [org.clojure/test.check "0.9.0"]
                  ; [criterium "0.4.3"]
 
                  ;; 14 clojure.spec
@@ -25,12 +16,5 @@
                  [prismatic/schema "1.0.4"]
                  [korma "0.4.3"]
                  [org.clojure/data.csv "0.1.3"]
-                 ;[org.clojure/test.check "0.9.0"]
-
-                 ;;[mysql/mysql-connector-java "6.0.5"]
-
-
-                 ;[org.clojure/clojure "1.8.0"]
-                 ;[org.clojure/core.async "0.3.465"]
 
                  ])
