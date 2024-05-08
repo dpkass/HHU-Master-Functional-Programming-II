6d2914ba73bb5161f0aa2df36739ffd32574768d
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2024-05-08 20:36:58 +0200

    Restructure - Each Week in own Folder

--- src/functional_programming_ii/main.clj
+++
@@ -1,5 +0,0 @@
-(ns functional-programming-ii.main)
-
-(defn -main [& args]
-  (println args)
-  0)
--- test/functional_programming_ii/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns functional-programming-ii.core-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [functional-programming-ii.core :as core]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 (core/foo 1)))))
